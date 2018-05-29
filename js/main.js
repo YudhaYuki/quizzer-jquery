37cff9ab99a13ac4577313276029be233e2deb7e
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-29 20:02:13 +0200

    jQuery - Click, animate and hide the button

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
 
-    $(document).click(function() {
+    (function startQuiz() {
+        
+        this.loadQuiz = function() {
 
-        $('.button').switchClass('button', 'otherClass', 1000);
-    
-    });
+            $('.panel_one h1').show("drop",500,function() {
+                $('.start_quiz').addClass("started", 500);
+            });
+
+            $(".start_quiz").on("click", function() {
+                showPanel(1);
+            });
+
+        };
+
+        this.showPanel = function(position) {
+            
+            var current = $('div[data-panel="'+ (position - 1) +'"]');
+
+            current.find('.wrapper').animate({left:"-=100px", opacity:0}, 500, function() {
+
+                current.addClass('hidden');
+
+            });
+
+        }
+
+
+
+        loadQuiz();
+
+    })();
 
 });
